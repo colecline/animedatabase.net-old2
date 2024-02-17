5acc3f490ed903301273d8de2b977752101545ef
Cole Cline <me@colecline.com>
2024-02-17 17:17:11 -0600

    feat: added and set default font to inter

--- animedatabase.net/src/app/layout.tsx
+++ animedatabase.net/src/app/layout.tsx
@@ -1,15 +1,23 @@
 import type { ReactNode } from "react";
-
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
 import "@/styles/globals.css";
 
 type RootLayoutProps = {
 	children: ReactNode;
 };
 
+export const fontSans = FontSans({
+	subsets: ["latin"],
+	variable: "--font-sans"
+});
+
 function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html>
-			<body>{children}</body>
+			<body className={cn("bg-background min-h-screen font-sans antialiased", fontSans.variable)}>
+				{children}
+			</body>
 		</html>
 	);
 }
